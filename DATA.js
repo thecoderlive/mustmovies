bef254b404cb9482edd1d90ee199d44bf710beb6
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-10 17:53:48 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,31 @@
+export const item = {
+    "id": "feeb92b2-492c-4f53-addb-7c187c6ed4f1",
+    "thumb_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/39.png",
+    "movies_title": "South positive body money itself should. Determine meet dark process standard language.",
+    "movies_count": "key",
+    "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/56.png",
+    "like_count": 8890,
+    "menu_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/125.png",
+    "movies_list": [
+        {
+            "id": "1e0065ca-308f-4fcb-8b5a-f6d51c3123fb",
+            "movies_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/218.png"
+        },
+        {
+            "id": "2923e1eb-dc62-4851-a8dc-35a66138a2fc",
+            "movies_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/85.png"
+        },
+        {
+            "id": "18a5d76c-a903-4e7d-8fe0-14bce4daa741",
+            "movies_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/65.png"
+        },
+        {
+            "id": "bc3bf200-8d16-4581-8f87-221940effd51",
+            "movies_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/150.png"
+        },
+        {
+            "id": "908bf775-0be7-4028-861b-6224f43afc00",
+            "movies_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/120.png"
+        }
+    ]
+}
